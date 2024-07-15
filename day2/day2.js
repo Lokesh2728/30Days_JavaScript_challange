f358dc530f3ab5d72787c84ce700037929109148
Lokesh2728 <101927859+Lokesh2728@users.noreply.github.com>
2024-07-15 15:55:05 +0000

    day2 completed

---
+++ day2/day2.js
@@ -0,0 +1,41 @@
+//operators in js
+let a=10
+let b=6
+
+//Arthematic operator
+console.log(a+b)  //addition
+console.log(a-b)  //sustraction
+console.log(a*b)  //multiplication
+console.log(a/b)  //division
+console.log(a%b)  //modulus
+console.log(a**b) //exponentiation
+
+//Assingment Operator
+let c=10
+let d=4
+console.log('Assingmet operator')
+console.log(c+=d) //a=a+b
+console.log(c-=d) //a=a-b
+console.log(c*=d) //a=a*b
+console.log(c/=d) //a=a/b
+console.log(c%=d) //a=a%b
+console.log(c **=d) //a=a**b
+
+//commparison operators
+let e=6
+let f='6'
+let g=7
+console.log('commparison operators')
+console.log(e==f) //true == work on value
+console.log(e===f) //false  work on datatypes
+console.log(e!=f) //false   work on value
+console.log(e!==f) //true   work on datatypes
+console.log(e>=g) //false
+console.log(e<=g) //true
+
+
+// ternary operator that chek number is positive or negetive and log the result
+let h=-10
+console.log('tenary operator')
+console.log((h>0)?'positive':'negetive')
+
